fc4902f6e02d3eb8069edca1ab5fe9fc2ad629d6
Barış Atasoy <batasoy@ford.com.tr>
2023-10-25 15:36:44 +0300

    -SUT -Testing with another data types

--- FakeNetworkUtility.Test/PingTest/NetworkServiceTest.cs
+++ FakeNetworkUtility.Test/PingTest/NetworkServiceTest.cs
@@ -3,6 +3,7 @@ using FluentAssertions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,14 +11,18 @@ namespace FakeNetworkUtility.Test.Ping
 {
     public class NetworkServiceTest
     {
+        private readonly NetworkService _pingService; 
+        public NetworkServiceTest()
+        {
+            // SUT(System Under Test)
+            _pingService = new NetworkService();
+        }
+        // Facts are tests which are always true. They test invariant conditions.
         [Fact]
         public void NetworkService_SendPing_ReturnsString()
         {
-            // arrange
-            var pingService = new NetworkService();
-
             // act
-            var result = pingService.SendPing();
+            var result = _pingService.SendPing();
 
             // assert
             result.Should().NotBeNullOrWhiteSpace();
@@ -26,20 +31,70 @@ namespace FakeNetworkUtility.Test.Ping
             result.Should().Contain("Success", Exactly.Once());
         }
 
+        // Theories are tests which are only true for a particular set of data.
         [Theory]
         [InlineData(1, 1, 2)]
         [InlineData(2, 2, 4)]
         public void NetworkService_PingTimeout_ReturnSumOfNums(int a, int b, int expected)
         {
-            // arrange
-            var pingService = new NetworkService();
-
             // act
-            var result = pingService.PingTimeout(a, b);
+            var result = _pingService.PingTimeout(a, b);
 
             // assert
             result.Should().Be(expected);
             result.Should().BeGreaterThanOrEqualTo(0);
+        }
+
+        [Fact]
+        public void NetworkService_LastPingDate_ReturnsDate()
+        {
+            var result = _pingService.LastPingDate();
+
+            // last ping date should be 5 seconds before now
+            result.Should().BeAfter(DateTime.Now.Add(new TimeSpan(0, 0, -5)));
+            // last ping date should be 5 seconds after now
+            result.Should().BeBefore(DateTime.Now.Add(new TimeSpan(0, 0, 5)));
+        }
+
+        [Fact]
+        public void NetworkService_GetPingOptions_ReturnsObject()
+        {
+            var expected = new PingOptions
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+
+            var result = _pingService.GetPingOptions();
+
+            result.Should().BeOfType<PingOptions>();
+            result.Should().BeEquivalentTo(expected);
+            result.Ttl.Should().Be(1);
+        }
+
+        [Fact]
+        public void NetworkService_MostRecentPings_ReturnsIEnumarable()
+        {
+            var expected = new PingOptions
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+
+            var result = _pingService.MostRecentPings();
+
+            result.Should().ContainEquivalentOf(expected);
+            result.Should().Contain(x => x.DontFragment == true);
+            foreach (var item in result)
+            {
+                item.Ttl.Should().BeGreaterThanOrEqualTo(0);
+                item.Ttl.Should().BeLessThanOrEqualTo(100);
+            }
+            for(int i = 0; i < result.Count(); i++)
+            {
+                if (i % 2 == 0) result.ElementAt(i).DontFragment.Should().Be(true);
+                else result.ElementAt(i).DontFragment.Should().Be(false);
+            }
         }
 
     }
--- FakeNetworkUtility/Ping/NetworkService.cs
+++ FakeNetworkUtility/Ping/NetworkService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,6 +17,48 @@ namespace FakeNetworkUtility.Ping
         public int PingTimeout(int a, int b)
         {
             return a + b;
+        }
+
+        public DateTime LastPingDate()
+        {
+            return DateTime.Now;
+        }
+
+        public PingOptions GetPingOptions()
+        {
+            return new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+        }
+
+        public IEnumerable<PingOptions> MostRecentPings()
+        {
+            IEnumerable<PingOptions> pingOptions = new[]
+            {
+                new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            },
+                new PingOptions()
+            {
+                DontFragment = false,
+                Ttl = 2
+            },
+                new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 7
+            },
+                new PingOptions()
+            {
+                DontFragment = false,
+                Ttl = 55
+            }
+        };
+            return pingOptions;
         }
     }
 }
